eeeaf002066904be15d46518366454f6394279f5
cyrus chen <pschen@lcs.on.ca>
2023-05-23 11:43:28 -0400

    Initial Commit

---
+++ AppleMusicClassical/AppleMusicClassicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicClassicalApp.swift
+//  AppleMusicClassical
+//
+//  Created by Cyrus Chen on 23/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicClassicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
